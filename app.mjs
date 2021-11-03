403f74d750130d10d5f811dbb0f97c0736d358c2
M.U$man <92122488+musmanmazhar@users.noreply.github.com>
2021-11-03 18:32:02 +0500

    Add files via upload

---
+++ app.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT|| 3000
+app.get("/karachi",(req,res)=> {
+    res.send("welcome back Mr Usman ");
+})
+app.get("/hello", (req, res) => {
+    res.send("hello world server");
+})
+app.get("/profile", (req, res) => {
+    res.send("my profile");
+})
+app.get("/", (req, res) => {
+    res.send("my bio");
+})
+app.listen(port, () => {
+    console.log("the port is runnng ")
+})
